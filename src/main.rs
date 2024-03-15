5849e43a61d8bb22ff09eaa0cf6b472bd55e7c6d
= <=>
2024-03-15 09:04:19 -0400

    tokio spawn

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,29 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
-use std::io::{ Write, Read };
+use tokio::net::{ TcpListener, TcpStream };
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::io::{ self, Write, Read };
 
-fn main() {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage
-    
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                eprintln!("New connection from: {}", stream.peer_addr().unwrap());
-                handle_stream(stream);
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let (mut stream, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            handle_stream(stream).await;
+        });
     }
+
+    Ok(())
 }
 
-fn handle_stream(mut stream: std::net::TcpStream) -> std::io::Result<()> {
+async fn handle_stream(mut stream: TcpStream) -> io::Result<()>  {
     loop {
         let mut buf = [0; 1024];
-        let client_message = stream.read(&mut buf)?;
+        let client_message = stream.read(&mut buf).await?;
 
         if client_message == 0 {
             eprintln!("Connection closed");
@@ -35,9 +32,7 @@ fn handle_stream(mut stream: std::net::TcpStream) -> std::io::Result<()> {
 
         eprintln!("Received: {}", String::from_utf8_lossy(&buf[..client_message]));
         let server_message = "+PONG\r\n".as_bytes();
-        stream.write_all(server_message)?;
+        stream.write_all(server_message).await?;
         eprintln!("Sent: {}", String::from_utf8_lossy(server_message));
     }
-    
-    Ok(())
 }
